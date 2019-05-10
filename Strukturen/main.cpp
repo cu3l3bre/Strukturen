b43c99dead8c0ecfb3344ade976c7ee164184c27
cu3l3bre <cu3l3bre@googlemail.com>
2019-05-10 11:00:44 +0200

    added a struct

--- Strukturen/main.cpp
+++ Strukturen/main.cpp
@@ -1,12 +1,42 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+// Definition einer Struktur
+
+struct Person
+{
+	string Vorname;
+	string Nachname;
+	char Geschlecht;
+	int Alter;
+	float Groesse;
+	bool Brille;
+
+};
+
 
 int main()
 {
 	cout << "Programm zu Strukturen" << endl << endl;
 
+	Person Person1;
 
+	Person1.Vorname = "Daniel";
+	Person1.Nachname = "Schmunkamp";
+	Person1.Geschlecht = 'm';
+	Person1.Alter = 32;
+	Person1.Groesse = 1.83;
+	Person1.Brille = false;
+
+
+	cout << Person1.Vorname << endl;
+	cout << Person1.Nachname << endl;
+	cout << Person1.Geschlecht << endl;
+	cout << Person1.Alter << endl;
+	cout << Person1.Groesse << endl;
+	cout << Person1.Brille << endl;
 
 
 
